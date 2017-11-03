47cf0afd3aff3ca6fe7abf89305feaa25bf7c592
Deng <Landau11th@users.noreply.github.com>
2017-11-03 23:58:13 +0800

    modification undergoing

--- GOAT_RK4.cpp
+++ GOAT_RK4.cpp
@@ -1,53 +1,47 @@
-#include <iostream>
-#include <complex>
-#include <armadillo>
 //#include <typeinfo>
 #include "GOAT_RK4.hpp"
 
 using namespace Deng::GOAT;
 
-template class Deng::GOAT::Hamiltonian<float>;
-template class Deng::GOAT::Hamiltonian<double>;
-template class Deng::GOAT::Hamiltonian<std::complex<float> >;
-template class Deng::GOAT::Hamiltonian<std::complex<double> >;
 
-template <typename Field>
-Hamiltonian<Field>::Hamiltonian(int N, int N_t, double tau, int dim_para)
+template class Deng::GOAT::Hamiltonian<float , float >;
+template class Deng::GOAT::Hamiltonian<double, double>;
+template class Deng::GOAT::Hamiltonian<std::complex<float> , float >;
+template class Deng::GOAT::Hamiltonian<std::complex<double>, double>;
+
+template <typename Field, typename Parameter>
+Hamiltonian<Field>::Hamiltonian(unsigned int N, unsigned int N_t, Parameter tau, unsigned int dim_para)
+	: _N(N), _N_t(N_t), _tau(tau), _dim_para(dim_para), _dt(tau/N_t)
 {
-    _dim_para = dim_para;
-    _tau = tau;
-    _N_t = N_t;
-    _dt = tau/N_t;
-    _N = N;
-
     parameters.set_size(_dim_para);
-    for(int i = 0; i < _dim_para; ++i)
+    for(unsigned int i = 0; i < _dim_para; ++i)
     {
         parameters[i] = 0;
     }
 }
-template <typename Field>
-Deng::Col_vector<arma::Mat<Field> > Hamiltonian<Field>::Derivative(Deng::Col_vector<arma::Mat<Field> > U, int t_index, bool half_time) const
+template <typename Field, typename Parameter>
+Deng::Col_vector<arma::Mat<Field>> Hamiltonian<Field>::Derivative(Deng::Col_vector<arma::Mat<Field>> U, unsigned int t_index, bool half_time) const
 {
-    double shift;
-    if(half_time)
-        shift = 0.5;
-    else
-        shift = 0;//0 or 1???
+    Parameter shift = half_time ? 0.5 : 0.0;
+    // if(half_time)
+        // shift = 0.5;
+    // else
+        // shift = 0;//0 or 1???
+	
+	//need to test the data type
+    Parameter t = (t_index + shift)*_dt;
+	
+	//+1 for the additional U with original time evolution
+    Deng::Col_vector<arma::Mat<Field>> k(_dim_para + 1);
 
-    double t = t_index*_dt + shift*_dt;
-
-    Deng::Col_vector<arma::Mat<Field> > k(_dim_para + 1);
-
-    Deng::Col_vector<arma::Mat<Field> > H_and_partial_H(_dim_para + 1);
-    H_and_partial_H = Dynamics(t);
-    //std::cout << "here?" << std::endl;
-    k[0] = H_and_partial_H[0]*U[0];
+    Deng::Col_vector<arma::Mat<Field>> iH_and_partial_H = Dynamics(t);
+    
+    k[0] = iH_and_partial_H[0]*U[0];
 
     for(int i = 1; i <= _dim_para; ++i)
     {
-		//std::cout << H_and_partial_H[i] << U[0] << H_and_partial_H[0] << U[i] << std::endl;
-		k[i] = H_and_partial_H[i]*U[0] + H_and_partial_H[0]*U[i];
+		//central equation of GOAT
+		k[i] = iH_and_partial_H[i]*U[0] + iH_and_partial_H[0]*U[i];
     }
 
     return k;
@@ -56,12 +50,10 @@ Deng::Col_vector<arma::Mat<Field> > Hamiltonian<Field>::Derivative(Deng::Col_vec
 
 
 
-
-
-template class Deng::GOAT::RK4<float>;
-template class Deng::GOAT::RK4<double>;
-template class Deng::GOAT::RK4<std::complex<float> >;
-template class Deng::GOAT::RK4<std::complex<double> >;
+template class Deng::GOAT::RK4<float , float >;
+template class Deng::GOAT::RK4<double, double>;
+template class Deng::GOAT::RK4<std::complex<float> , float >;
+template class Deng::GOAT::RK4<std::complex<double>, double>;
 
 /*
 template <typename Field>
@@ -77,51 +69,8 @@ RK4<Field>::RK4(int dim_para, double tau, int N_t)
     final_state.Constructor(_dim_para);
 }
 */
-template <typename Field>
-void RK4<Field>::Evolve_one_step(const Deng::GOAT::Hamiltonian<Field> &H, const int t_index)
-{
-    //Deng::Col_vector<arma::Mat<Field> > temp_state(H._dim_para + 1);
-    //std::cout << "here0" << std::endl;
-    Deng::Col_vector<arma::Mat<Field> > k1 = H.Derivative(current_state               , t_index    , false);
-
-    //temp_state = current_state + 0.5*H._dt*k1;
-    Deng::Col_vector<arma::Mat<Field> > k2 = H.Derivative(current_state + 0.5*H._dt*k1, t_index    , true );
-
-    //temp_state = current_state + 0.5*H._dt*k2;
-    Deng::Col_vector<arma::Mat<Field> > k3 = H.Derivative(current_state + 0.5*H._dt*k2, t_index    , true );
-
-    //temp_state = current_state +     H._dt*k3;
-    Deng::Col_vector<arma::Mat<Field> > k4 = H.Derivative(current_state +     H._dt*k3, t_index + 1, false);
-
-    next_state = current_state + (H._dt/6.0)*(k1 + 2.0*k2 + 2.0*k3 + k4);
-
-    /*
-    //temp_state = current_state + k1*(H._dt*0.5);
-    //Deng::Col_vector<arma::Mat<Field> > k2 = H.Derivative(temp_state    , t_index  , true);
-
-    //temp_state = current_state + k2*(H._dt*0.5);
-    //Deng::Col_vector<arma::Mat<Field> > k3 = H.Derivative(temp_state    , t_index  , true );
-
-    //temp_state = current_state + k3*(H._dt    );
-    //Deng::Col_vector<arma::Mat<Field> > k4 = H.Derivative(temp_state    , t_index+1, false);
-
-    //next_state = current_state + (k1 + k2*2.0 + k3*2.0 + k4)*(H._dt/6.0);
-    */
-
-    //std::cout << "at" << t_index << std::endl;
-
-}
-template <typename Field>
-void RK4<Field>::Evolve_to_final(const Deng::GOAT::Hamiltonian<Field> &H)
-{
-    for(int i = 0; i < H._N_t; ++i)
-    {
-        Evolve_one_step(H, i);
-        current_state = next_state;
-    }
-}
-template <typename Field>
-void RK4<Field>::Prep_for_H(const Deng::GOAT::Hamiltonian<Field> &H)
+template <typename Field, typename Parameter>
+void RK4<Field>::Prep_for_H(const Deng::GOAT::Hamiltonian<Field, Parameter> &H)
 {
     //make sure the dimension fits
     current_state.set_size(H._dim_para + 1);
@@ -133,8 +82,30 @@ void RK4<Field>::Prep_for_H(const Deng::GOAT::Hamiltonian<Field> &H)
         current_state[i].zeros(H._N, H._N);
         next_state[i].zeros(H._N, H._N);
     }
-    //_eyedentity.eye(H._N, H._N);
 }
+template <typename Field, typename Parameter>
+void RK4<Field>::Evolve_one_step(const Deng::GOAT::Hamiltonian<Field, Parameter> &H, const unsigned int t_index)
+{
+    auto k1 = H.Derivative(current_state               , t_index    , false);
+
+    auto k2 = H.Derivative(current_state + 0.5*H._dt*k1, t_index    , true );
+
+    auto k3 = H.Derivative(current_state + 0.5*H._dt*k2, t_index    , true );
+
+    auto k4 = H.Derivative(current_state +     H._dt*k3, t_index + 1, false);
+	//armadillo will throw runtime error if dimention does not fit here
+    next_state = current_state + (H._dt/6.0)*(k1 + 2.0*k2 + 2.0*k3 + k4);
+}
+template <typename Field, typename Parameter>
+void RK4<Field>::Evolve_to_final(const Deng::GOAT::Hamiltonian<Field, Parameter> &H)
+{
+    for(int i = 0; i < H._N_t; ++i)
+    {
+        Evolve_one_step(H, i);
+        current_state = next_state;
+    }
+}
+
 
 
 
--- GOAT_RK4.hpp
+++ GOAT_RK4.hpp
@@ -11,56 +11,56 @@ namespace Deng
 {
     namespace GOAT
     {
-        template <typename Field>
+        template <typename Field, typename Parameter>
         class RK4;
 
         //the abstract base class for (almost) all Hamiltonians using GOAT
-        template <typename Field>
+        template <typename Field, typename Parameter>
         class Hamiltonian//defined the most basic Hamiltonian, with only dimension, time parameters and the parameter space dimension
         {
         protected:
             //for the derived class to use
-            int _dim_para;//dimension of parametric space
-            double _dt;
-            double _tau;//total time
-            int _N_t;//# of time steps
-            int _N;//dimension of state space
+            const unsigned int _dim_para;//dimension of parametric space
+            const Parameter _dt;
+            const Parameter _tau;//total time
+            const unsigned int _N_t;//# of time steps
+            const unsigned int _N;//dimension of state space
         public:
             //control parameters
             //have to use double as the parameters are usually real
-            mutable arma::Col<double> parameters;
-            virtual void Update_parameters(arma::Col<double> new_para)
+            mutable arma::Col<Parameter> parameters;
+            virtual void Update_parameters(arma::Col<Parameter> new_para) const
             {
                 parameters = new_para;
             };
             //input necessary parameters
-            Hamiltonian(int N, int N_t, double tau, int dim_para = 0);
+            Hamiltonian(unsigned int N, unsigned int N_t, Parameter tau, unsigned int dim_para = 0);
             //return the protected data
             //int dimension_of_parametric_space() const {return _dim_para;};
             //int total_steps_in_time() const {return _N_t;};
             //int dimension_of_state_space() const {return _N;};
             //double total_time() const {return _dt;};
             //calculate the derivative of U in a block vector form
-            virtual Deng::Col_vector<arma::Mat<Field> > Derivative (Deng::Col_vector<arma::Mat<Field> > position, int t_index, bool half_time) const;
+            virtual Deng::Col_vector<arma::Mat<Field>> Derivative (Deng::Col_vector<arma::Mat<Field> > position, unsigned int t_index, bool half_time) const;
             //invoked by member function Derivative; return H and partial_H in a block vector form
             //pure-virtual function, must be implemented in instances!!!!
-            virtual Deng::Col_vector<arma::Mat<Field> > Dynamics(double t) const = 0;
+            virtual Deng::Col_vector<arma::Mat<Field>> Dynamics(Parameter t) const = 0;
             //empty virtual destructor
             virtual ~Hamiltonian() = default;
             //for RK4 and conjugate gradient to use the parameters
             friend class RK4<Field>;
             friend class Conjugate_Gradient_Method;
         };
-
-        template <typename Field>
-        class RK4//Runge-Kutta 4th order, applied to GOAT method only, as the time evolution of U is sparse under GOAT
+		
+		//RK4 specialized for GOAT
+        template <typename Field, typename Parameter>
+        class RK4//Runge-Kutta 4th order, applied to GOAT method only
+				 //as the time evolution of U is sparse and has fiexd format under GOAT
         {
-        private:
-            //arma::Mat<Field> _eyedentity;
         public:
             //state to be evolved
-            Deng::Col_vector<arma::Mat<Field> > current_state;
-            Deng::Col_vector<arma::Mat<Field> > next_state;
+            Deng::Col_vector<arma::Mat<Field>> current_state;
+            Deng::Col_vector<arma::Mat<Field>> next_state;
             //Deng::Col_vector<arma::Mat<Field> > initial_state;
             //Deng::Col_vector<arma::Mat<Field> > final_state;
 
@@ -72,8 +72,8 @@ namespace Deng
             //initialize initial states and identity
             virtual void Prep_for_H(const Deng::GOAT::Hamiltonian<Field> &H);
             //we use the keyword const here to prevent changing H
-            virtual void Evolve_one_step(const Deng::GOAT::Hamiltonian<Field> &H, const int t_index);
-            virtual void Evolve_to_final(const Deng::GOAT::Hamiltonian<Field> &H);
+            virtual void Evolve_one_step(const Deng::GOAT::Hamiltonian<Field, Parameter> &H, const unsigned int t_index);
+            virtual void Evolve_to_final(const Deng::GOAT::Hamiltonian<Field, Parameter> &H);
 
         };
 
